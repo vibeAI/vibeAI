82e7318d398175fc2a3680ddf38f6e86dff495ce
Marcelo Velasco <marceluno10@gmail.com>
2023-01-24 15:07:58 +0100

    second commit

--- client/src/components/SpotifyArtist.js
+++ client/src/components/SpotifyArtist.js
@@ -6,22 +6,35 @@ import { useEffect, useState } from "react";
 export default function SpotifyArtist() {
 
     const [uri, setUri] = useState("");
+    const [artist, setArtist] = useState("");
 
-
-      useEffect(() => {
-        apiClient.get("extremoduro").then((response) => {
-          setUri(response.data.artist.uri);
+    useEffect(() => {
+        apiClient.get(`?q=extremoduro&type=artist`).then((response, error) => {
+            // setUri(response.data.artist);
+            console.log(response);
+            if(error) throw error
         });
 
-      }, []);
+
+    }, [artist]);
+
+    function handleArtistChange(e) {
+        setArtist("extremoduro");
+
+    }
 
 
-    return (
-        <>
-            <div>
-                <h1>{uri}</h1>
-            </div>
-        </>
-    )
+        return (
+            <>
+                <div>
+                    <button onClick={(e) => {
+                        handleArtistChange(e)
 
-}
+                    }
+                    }>dadsa</button>
+
+                </div>
+            </>
+        )
+
+    }
