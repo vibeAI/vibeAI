ba03c7faf44d0bc6478de00a072f6b160c7f873a
Marcelo Velasco <marceluno10@gmail.com>
2023-01-25 16:31:56 +0100

    fas

--- client/src/components/SpotifyArtist.js
+++ client/src/components/SpotifyArtist.js
@@ -2,33 +2,44 @@ import apiClient from "../spotifyAuth/spotify";
 import { useEffect, useState } from "react";
 import Spotify from "react-spotify-embed";
 
-export default function SpotifyArtist() {
+export default function SpotifyArtist(recomendacion1, recomendacion2, recomendacion3) {
 
     const [uri, setUri] = useState("");
+    const [uri2, setUri2] = useState("");
+    const [uri3, setUri3] = useState("");
     const [artist, setArtist] = useState("");
+    const [artist2, setArtist2] = useState("");
+    const [artist3, setArtist3] = useState("");
 
-    //ESPERANDO A ENDPOINT
-
-    // useEffect(() => {
-    //     fetch("").then((response) => {
-    //         const artist = response.data.name
-    //         setArtist(artist)
-
-    //     });
-
-
-    // }, []);
 
     useEffect(() => {
+
+        setArtist(recomendacion1);
+        setArtist2(recomendacion2);
+        setArtist3(recomendacion3);
+        
+
         apiClient.get(`?query=${artist}&type=artist&locale=es-ES%2Ces%3Bq%3D0.9&offset=0&limit=20`).then((response) => {
 
             const uri = response.data.artists.items[0].uri.split(":")
             setUri(uri[2])
         });
-    }, [artist]);
+        apiClient.get(`?query=${artist2}&type=artist&locale=es-ES%2Ces%3Bq%3D0.9&offset=0&limit=20`).then((response) => {
+
+            const uri = response.data.artists.items[0].uri.split(":")
+            setUri2(uri[2])
+        });
+        apiClient.get(`?query=${artist3}&type=artist&locale=es-ES%2Ces%3Bq%3D0.9&offset=0&limit=20`).then((response) => {
+
+            const uri = response.data.artists.items[0].uri.split(":")
+            setUri3(uri[2])
+        });
+    }, [artist, artist2, artist3]);
 
     function handleArtistChange(e) {
-        setArtist("michaeljackson");
+        setArtist("elpayojuanmanuel");
+        setArtist2("losdelrio")
+        setArtist3("julius")
 
     }
 
@@ -43,8 +54,8 @@ export default function SpotifyArtist() {
 
             <div style={{ display: "flex", justifyContent: "space-around" }}>
                 <Spotify link={`https://open.spotify.com/artist/${uri}?si=4472348a63dd4f83`} />
-                <Spotify link={`https://open.spotify.com/artist/${uri}?si=4472348a63dd4f83`} />
-                <Spotify link={`https://open.spotify.com/artist/${uri}?si=4472348a63dd4f83`} />
+                <Spotify link={`https://open.spotify.com/artist/${uri2}?si=4472348a63dd4f83`} />
+                <Spotify link={`https://open.spotify.com/artist/${uri3}?si=4472348a63dd4f83`} />
             </div>
         </>
     )
