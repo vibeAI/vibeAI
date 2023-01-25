8323e82b108851e33f3d562b3caa9c5ccff6da7a
Rafa Cabezas <rafa.cabezas.00@gmail.com>
2023-01-25 13:10:11 +0100

    segundo intento home

--- client/src/components/SpotifyArtist.js
+++ client/src/components/SpotifyArtist.js
@@ -2,15 +2,12 @@ import apiClient from "../spotifyAuth/spotify";
 import { useEffect, useState } from "react";
 import Spotify from "react-spotify-embed";
 
-
-
 export default function SpotifyArtist() {
 
     const [uri, setUri] = useState("");
     const [artist, setArtist] = useState("");
 
-
-//ESPERANDO A ENDPOINT
+    //ESPERANDO A ENDPOINT
 
     // useEffect(() => {
     //     fetch("").then((response) => {
@@ -22,26 +19,18 @@ export default function SpotifyArtist() {
 
     // }, []);
 
-
     useEffect(() => {
         apiClient.get(`?query=${artist}&type=artist&locale=es-ES%2Ces%3Bq%3D0.9&offset=0&limit=20`).then((response) => {
 
             const uri = response.data.artists.items[0].uri.split(":")
             setUri(uri[2])
-            
-
         });
-
-
     }, [artist]);
-
-
 
     function handleArtistChange(e) {
         setArtist("michaeljackson");
 
     }
-
 
     return (
         <>
@@ -52,7 +41,7 @@ export default function SpotifyArtist() {
                 }>BOTON</button>
             </div>
 
-            <div  style={{ display: "flex", justifyContent: "space-around" }}>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
                 <Spotify link={`https://open.spotify.com/artist/${uri}?si=4472348a63dd4f83`} />
                 <Spotify link="https://open.spotify.com/artist/7hJcb9fa4alzcOq3EaNPoG?si=4472348a63dd4f83" />
                 <Spotify link="https://open.spotify.com/artist/5Y30WvST5ZHhQeemywiT9B?si=4472348a63dd4f83" />
