dbd95b7a0787b1b38ac5e518fafae3c3d99f106d
Neivajg <114397051+Neivajg@users.noreply.github.com>
2023-01-25 13:28:05 +0100

    Styles.css,aboutUs.css y Pages AboutUs

--- client/src/pages/aboutUs.js
+++ client/src/pages/aboutUs.js
@@ -1,11 +1,85 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-import "../styles/styles.css"
+import "../styles/aboutUs.css"
 
 const AboutUs = () => {
   return (
     <>
-      <h2 style={{color: 'white'}}>AboutUs</h2>
+    <div className='containerCards'>
+      <Card className='cardAboutUs' style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Neiva Jiménez</Card.Title>
+          <Button variant="primary">Linkedin</Button>
+        </Card.Body>
+      </Card>
+
+      <Card className='cardAboutUs' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Rafa Cabezas</Card.Title>
+        <Button variant="primary">Linkedin</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='cardAboutUs' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Dani Valero</Card.Title>
+        <Button variant="primary">Linkedin</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='cardAboutUs' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title> Ruben Ganga</Card.Title>
+        <Button variant="primary">Linkedin</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='cardAboutUs' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Marcelo </Card.Title>
+        <Button variant="primary">Linkedin</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='cardAboutUs' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Génesis</Card.Title>
+        <Button variant="primary">Linkedin</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='cardAboutUs' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Alberto</Card.Title>
+        <Button variant="primary">Linkedin</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='cardAboutUs' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Ma</Card.Title>
+        <Button variant="primary">Linkedin</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='cardAboutUs' style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>sss</Card.Title>
+        <Button variant="primary">Linkedin</Button>
+      </Card.Body>
+    </Card>
+    </div>
     </>
   );
 };
--- client/src/pages/landingPage.js
+++ client/src/pages/landingPage.js
@@ -9,7 +9,7 @@ const LandingPage = () => {
     return (
         <>
             <div className='body'>
-                <div className='card'>
+                <div className='cardLanding'>
                     <h1 className='title'>The final Music Recommendation System</h1>
                     <br></br>
                     <p className='text'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
