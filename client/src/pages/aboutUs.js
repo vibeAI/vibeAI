44becae47f544569e5ddb83d1d34e6d9e4a46da6
Neivajg <114397051+Neivajg@users.noreply.github.com>
2023-01-26 13:14:05 +0100

    AboutUs:esperando linkedin y avatares para insertar. Responsive OK

--- client/src/pages/aboutUs.js
+++ client/src/pages/aboutUs.js
@@ -1,86 +1,99 @@
 import React from 'react'
-
+import marcelo from '../assets/Marcelo.png'
 
 import "../styles/aboutUs.css"
 
 const AboutUs = () => {
-  return (
-    
-     <div className='containerCards'>
-      <div className="card">
-        <div className="img">
-          <img src='' alt='Antonio Ambros'></img>
-        </div>
-        <span> Antonio Ambros</span>
-        <button>Linkedin</button>
-      </div>
 
+
+  return (
+<>
+   
+    <h2 className='titulos'>Full Stack</h2>
+    <div className='containerCards'>
+     
       <div className="card">
         <div className="img">
-          <img src='' alt='Dani Valero'></img>
+          <img src={marcelo}  alt='Dani Valero'></img>
         </div>
         <span> Dani Valero</span>
-        <button>Linkedin</button>
+        <a href='' target="_blank"> <button  >Linkedin</button></a>
       </div>
+      
 
       <div className="card">
         <div className="img">
-          <img src='' alt='Génesis Rojas'></img>
-        </div>
-        <span> Génesis Rojas </span>
-        <button>Linkedin</button>
-      </div>
-
-      <div className="card">
-        <div className="img">
-          <img src='' alt='Guillermo Martín'></img>
-        </div>
-        <span> Guillermo Martín</span>
-        <button>Linkedin</button>
-      </div>
-
-      <div className="card">
-        <div className="img">
-          <img src='' alt='Marcelo Velasco'></img>
+          <img src={marcelo}  alt='Marcelo Velasco'></img>
         </div>
         <span> Marcelo Velasco</span>
-        <button>Linkedin</button>
+        <a href='https://www.linkedin.com/in/marcelovelasco/' target="_blank"> <button  >Linkedin</button></a>
       </div>
 
       <div className="card">
         <div className="img">
-          <img src='' alt='María de los Ángeles Naranjo'></img>
-        </div>
-        <span> María de los Ángeles Naranjo</span>
-        <button>Linkedin</button>
-      </div>
-
-   
-      <div className="card">
-        <div className="img">
-          <img src='' alt='Neiva Jiménez'></img>
+          <img src={marcelo}  alt='Neiva Jiménez'></img>
         </div>
         <span> Neiva Jiménez</span>
-        <button>Linkedin</button>
+        <a href='https://www.linkedin.com/in/neiva-jim%C3%A9nez-gonz%C3%A1lez' target="_blank"> <button  >Linkedin</button></a>
       </div>
 
-      <div className="card">
+      <div  className="card">
         <div className="img">
-          <img src='' alt=' Rafa Cabezas'></img>
+          <img src={marcelo}  alt=' Rafa Cabezas'></img>
         </div>
         <span> Rafa Cabezas</span>
-        <button>Linkedin</button>
+        <a href='' target="_blank"> <button  >Linkedin</button></a>
       </div>
 
-      <div className="card">
+      <div  className="card">
         <div className="img">
-          <img src='' alt='Rubén Ruiz'></img>
+          <img src={marcelo}  alt='Rubén Ruiz'></img>
         </div>
         <span> Rubén Ruiz</span>
-        <button>Linkedin</button>
+        <a href='' target="_blank"> <button  >Linkedin</button></a>
       </div>
-  
+
     </div>
+   
+
+       <h2 className='titulos'>Data Science</h2>
+    <div  className='containerCards'>
+       <div className="card">
+         <div className="img">
+           <img src={marcelo}  alt='Antonio Ambros'></img>
+         </div>
+         <span> Antonio Ambros</span>
+         <a href='' target="_blank"> <button  >Linkedin</button></a>
+       </div>
+
+       <div className="card">
+         <div className="img">
+           <img src={marcelo}  alt='Génesis Rojas'></img>
+         </div>
+         <span> Génesis Rojas </span>
+         <a href='' target="_blank"> <button  >Linkedin</button></a>
+       </div>
+
+       <div className="card">
+         <div className="img">
+           <img src={marcelo}  alt='Guillermo Martín'></img>
+         </div>
+         <span> Guillermo Martín</span>
+         <a href='https://www.linkedin.com/in/guillermoordaz/' target="_blank"> <button  >Linkedin</button></a>
+       </div>
+
+
+       <div className="card">
+         <div className="img">
+           <img src={marcelo}  alt='María de los Ángeles Naranjo'></img>
+         </div>
+         <span> María de los Ángeles Naranjo</span>
+         <a href='' target="_blank"> <button  >Linkedin</button></a>
+       </div>
+
+   
+     </div>
+     </>
   );
 };
 
