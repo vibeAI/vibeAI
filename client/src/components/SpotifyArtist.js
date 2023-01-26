9a114097062016bff637029d6438681ebf5f5463
Marcelo Velasco <marceluno10@gmail.com>
2023-01-26 12:20:45 +0100

    boton endpoint y svg

--- client/src/components/SpotifyArtist.js
+++ client/src/components/SpotifyArtist.js
@@ -1,6 +1,10 @@
 import apiClient from "../spotifyAuth/spotify";
 import { useEffect, useState } from "react";
 import Spotify from "react-spotify-embed";
+import "../assets/check.svg"
+import "./styles/spotifyArtist_styles.css"
+import "../assets/wrong.svg"
+
 
 export default function SpotifyArtist(props) {
 
@@ -36,6 +40,15 @@ export default function SpotifyArtist(props) {
                 <Spotify link={`https://open.spotify.com/artist/${uri}?si=4472348a63dd4f83`} />
                 <Spotify link={`https://open.spotify.com/artist/${uri2}?si=4472348a63dd4f83`} />
                 <Spotify link={`https://open.spotify.com/artist/${uri3}?si=4472348a63dd4f83`} />
+            </div>
+            <div className="likeordislike">
+                <button className="like">
+
+                    <svg height="24" width="24" xmlns={"../assets/check.svg"} viewBox="0 0 512 512"><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" /></svg>
+                </button>
+                <button className="dislike">
+                    <svg height="24" width="24" xmlns={"../assets/wrong.svg"} viewBox="0 0 320 512"><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>
+                </button>
             </div>
         </>
     )
