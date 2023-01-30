ed0695b94cfcd34da3da40b338946b51bc80e728
Dani Valero <danivalero98@hotmail.com>
2023-01-30 11:02:19 +0100

    Modificaciones menores al perfil, falta por arreglar envio del form

--- client/src/components/EditUserForm.js
+++ client/src/components/EditUserForm.js
@@ -9,29 +9,54 @@ const EditUserForm = () => {
     const [newEmail, setNewEmail] = useState("")
     const [newPassword, setNewPassword] = useState("")
     const [newPassword2, setNewPassword2] = useState("")
+    const users = user.getCurrentUser().data
+    const username = user.getCurrentUser()
 
     const handleForm = async (e) => {
         e.preventDefault()
-        // console.log(user.getCurrentUser().email)
-        // console.log(user.getCurrentUser().username)
+        
+       newPassword === "" && toast.warning("Password is required")
+       newPassword !== newPassword2 && toast.warning("Passwords must match!")
+        
+       newUsername === "" && setNewUsername(username.username)
+       newEmail === "" && setNewEmail(username.email)
+
+       fetch(`http://www.localhost:3000/user/edit/${user.getCurrentUser().email}`, 
+        
+       {method: 'PUT',
+        headers: {
+         'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: newUsername,
+            email: newEmail,
+            password: newPassword
+        }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data) && toast.info("Account information updated successfully")
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        })
     }
-     const users = user.getCurrentUser().data
-     const username = user.getCurrentUser()
+     
 
     return (
        
         <>
             <h1 className='tituloSaludo'>Hello {username.username}</h1>
             <div>
-                <h1 className='tituloRecomendaciones'>Recent recommendations:</h1>
+                <h1 className='tituloRecomendaciones'>Here are your recent recommendations:</h1>
                 <div className='ContainerProfile'>
                     
                         {users.reverse().map((e)=>(
                             <div className='cardProfile'>
-                                <h3>Grupos</h3>
+                                <h3>Artist/Bands</h3>
                                 <h4>1: {e.grupo1}</h4>
                                 <h4>2: {e.grupo2}</h4>
-                                <h3>Recomendaciones:</h3>
+                                <h3>Recommendations:</h3>
                                 <h4> 1: {e.recomendacion1}</h4>
                                 <h4> 2: {e.recomendacion2}</h4>
                                 <h4> 3: {e.recomendacion3}</h4>
@@ -41,7 +66,7 @@ const EditUserForm = () => {
                  
             </div>
             <div className='login-wrapper'>
-            <h1 className='login-title'>change Password</h1>
+            <h1 className='login-title'>Edit your information:</h1>
             <form className='inputbox loginForm'>
                 <div className="inputbox">
                     <input autoComplete='true' type="text" id="username" name="username" onChange={(e) => setNewUsername(e.currentTarget.value)} />
